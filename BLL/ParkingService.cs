cb7cac02409234b22336a63505243fcb18d287f4
Shershavcik <davidnjg6@gmail.com>
2025-10-08 14:27:09 +0300

    Add files via upload

---
+++ BLL/ParkingService.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Data;
+using ParkingSystem.DAL;
+
+namespace ParkingSystem.BLL
+{
+    public class ParkingService
+    {
+        private readonly VehicleOwnerDAL ownerDAL = new();
+        private readonly VehicleDAL vehicleDAL = new();
+        private readonly ParkingDAL parkingDAL = new();
+
+        public void AddOwner(string f, string l, string p, string? e)
+        {
+            if (string.IsNullOrWhiteSpace(f) || string.IsNullOrWhiteSpace(l))
+                throw new Exception("Имя и фамилия обязательны");
+            ownerDAL.Add(f, l, p, e);
+        }
+
+        public void AddVehicle(string plate, string make, string model, string color, int ownerId)
+        {
+            if (string.IsNullOrWhiteSpace(plate))
+                throw new Exception("Госномер обязателен");
+            vehicleDAL.Add(plate, make, model, color, ownerId);
+        }
+
+        public DataTable GetAllOwners() => ownerDAL.GetAll();
+        public DataTable GetAllVehicles() => vehicleDAL.GetAll();
+        public DataTable GetAllSpaces() => parkingDAL.GetSpaces();
+        public DataTable GetTickets() => parkingDAL.GetTickets();
+
+        public void StartParking(int vehicleId, string spaceNumber) => parkingDAL.StartParking(vehicleId, spaceNumber);
+        public void EndParking(int ticketId) => parkingDAL.EndParking(ticketId);
+    }
+}
+
---
+++ DAL/VehicleDAL.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ParkingSystem.DAL
+{
+    public class VehicleDAL
+    {
+        public void Add(string plate, string make, string model, string color, int ownerId)
+        {
+            var sql = @"INSERT INTO Vehicle (LicensePlate, Make, Model, Color, OwnerID)
+                        VALUES (@lp,@mk,@md,@cl,@oid)";
+            DbHelper.ExecuteNonQuery(sql,
+                new SqlParameter("@lp", plate),
+                new SqlParameter("@mk", make),
+                new SqlParameter("@md", model),
+                new SqlParameter("@cl", color),
+                new SqlParameter("@oid", ownerId));
+        }
+
+        public DataTable GetAll()
+        {
+            return DbHelper.GetDataTable(@"
+                SELECT v.VehicleID, v.LicensePlate, v.Make, v.Model, v.Color, 
+                       o.FirstName + ' ' + o.LastName AS Owner
+                FROM Vehicle v 
+                JOIN VehicleOwner o ON v.OwnerID=o.OwnerID
+                ORDER BY v.LicensePlate");
+        }
+    }
+}
+
